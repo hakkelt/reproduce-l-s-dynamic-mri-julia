d1b445de5b4b1945a6857fe7a10b5f8a7f962542
hakkelt <hakkelt@gmail.com>
2020-04-26 23:12:59 +0200

    LinearMaps optimized version

--- helper_functions.jl
+++ helper_functions.jl
@@ -71,7 +71,7 @@ function E_basis(basis::String;
     Bi
 end
 
-# Note that this function in not fully tested, it is just the raw transcription of the getEnufft Matlab function to Julia. I left it here for sake of completeness, but it the notebooks, I used a simplified version specialized for the given task.
+# Note that this function in not fully tested, it is just the raw transcription of the getEnufft Matlab function to Julia. I left it here for sake of completeness, but in the notebooks, I used a simplified version specialized for the given task.
 function getEnufft_original(sense_maps::Array{Complex{Float64},3};
     nx::Int64 = size(sense_maps,1), ny::Int64 = size(sense_maps,2), nt::Int64,
     nc::Int64 = size(sense_maps,3), mask::Union{BitArray{3},Nothing} = nothing,
@@ -182,3 +182,187 @@ fftshift!(output::AbstractArray, input::AbstractArray, dims::Int) =
 
 ifftshift!(output::AbstractArray, input::AbstractArray, dims::Int) =
     ifftshift!(output, input, (dims,))
+
+using NFFT, MIRT, EllipsisNotation
+
+function nufft_plan(w::AbstractMatrix{<:Real}, N::Dims;
+        n_shift::AbstractVector{<:Real} = zeros(Int, length(N)),
+        nfft_m::Int = 4,
+        nfft_sigma::Real = 1.25,
+        pi_error::Bool = true,
+        do_many::Bool = true)
+
+    any(N .< 6) && throw("NFFT may be erroneous for small N")
+    any(isodd.(N)) && throw("NFFT erroneous for odd N")
+    pi_error && any(abs.(w) .> π) && throw(ArgumentError("|w| > π is likely an error"))
+
+    M,D = size(w)
+    length(N) != D && throw(DimensionMismatch("length(N) vs D=$D"))
+    length(n_shift) != D && throw(DimensionMismatch("length(n_shift) vs D=$D"))
+
+    T = MIRT.nufft_eltype(w)
+    CT = Complex{T}
+    f = convert.(T, w/(2π)) # note: NFFTPlan must have correct type
+    p = NFFTPlan(f', N, nfft_m, nfft_sigma) # create plan
+
+    # extra phase here because NFFT always starts from -N/2
+    phasor = CT.(cis.(-w * (collect(N)/2. - n_shift)))
+    phasor_conj = conj.(phasor)
+    phasor_buffer = similar(phasor_conj)
+    forw1! = (b, x) -> b .= NFFT.nfft!(p, MIRT.nufft_typer(CT, x), b) .* phasor
+    backw1! = (b, y) -> NFFT.nfft_adjoint!(p, MIRT.nufft_typer(CT, phasor_buffer .= y .* phasor_conj), b)
+    
+    if do_many
+        forw! = (b, x) -> begin
+                for i in axes(x, ndims(x))
+                    @views forw1!(b[.., i], x[.., i])
+                end
+            end
+        backw! = (b, y) -> begin
+                for i in axes(y, ndims(y))
+                    @views backw1!(b[.., i], y[.., i])
+                end
+            end
+    else
+        forw! = forw1!
+        backw! = backw1!
+    end
+
+    return (nufft=forw!, nufft_adjoint=backw!)
+end
+
+import LinearAlgebra.LAPACK
+
+import LinearAlgebra.BLAS.@blasfunc
+
+import LinearAlgebra: BlasFloat, BlasInt, LAPACKException,
+    DimensionMismatch, SingularException, PosDefException, chkstride1, checksquare
+
+using LinearAlgebra: triu, tril, dot
+
+using Base: iszero, require_one_based_indexing
+
+for (geev, gesvd, gesdd, ggsvd, elty, relty) in
+    ((:dgeev_,:dgesvd_,:dgesdd_,:dggsvd_,:Float64,:Float64),
+     (:sgeev_,:sgesvd_,:sgesdd_,:sggsvd_,:Float32,:Float32),
+     (:zgeev_,:zgesvd_,:zgesdd_,:zggsvd_,:ComplexF64,:Float64),
+     (:cgeev_,:cgesvd_,:cgesdd_,:cggsvd_,:ComplexF32,:Float32))
+    @eval begin
+        #    SUBROUTINE DGESDD( JOBZ, M, N, A, LDA, S, U, LDU, VT, LDVT, WORK,
+        #                   LWORK, IWORK, INFO )
+        #*     .. Scalar Arguments ..
+        #      CHARACTER          JOBZ
+        #      INTEGER            INFO, LDA, LDU, LDVT, LWORK, M, N
+        #*     ..
+        #*     .. Array Arguments ..
+        #      INTEGER            IWORK( * )
+        #      DOUBLE PRECISION   A( LDA, * ), S( * ), U( LDU, * ),
+        #                        VT( LDVT, * ), WORK( * )
+        function gesdd!(job::AbstractChar, A::AbstractMatrix{$elty},
+                U::AbstractMatrix{$elty}, S::AbstractVector{$relty}, VT::AbstractMatrix{$elty})
+            require_one_based_indexing(A)
+            chkstride1(A)
+            m, n   = size(A)
+            minmn  = min(m, n)
+            
+            #=if job == 'A'
+                U  = similar(A, $elty, (m, m))
+                VT = similar(A, $elty, (n, n))
+            elseif job == 'S'
+                U  = similar(A, $elty, (m, minmn))
+                VT = similar(A, $elty, (minmn, n))
+            elseif job == 'O'
+                U  = similar(A, $elty, (m, m >= n ? 0 : m))
+                VT = similar(A, $elty, (n, m >= n ? n : 0))
+            else
+                U  = similar(A, $elty, (m, 0))
+                VT = similar(A, $elty, (n, 0))
+            end=#
+            
+            if job == 'A'
+                @assert size(U) == (m, m)
+                @assert size(VT) == (n, n)
+            elseif job == 'S'
+                @assert size(U) == (m, minmn)
+                @assert size(VT) == (minmn, n)
+            elseif job == 'O'
+                @assert size(U) == (m,  m >= n ? 0 : m)
+                @assert size(VT) == (n, m >= n ? n : 0)
+            else
+                @assert size(U) == (m, 0)
+                @assert size(VT) == (n, 0)
+            end
+            @assert eltype(U) <: $elty
+            @assert eltype(VT) <: $elty
+            work   = Vector{$elty}(undef, 1)
+            lwork  = BlasInt(-1)
+            @assert size(S, 1) == minmn
+            @assert eltype(S) <: $relty
+            #S      = similar(A, $relty, minmn)
+            cmplx  = eltype(A)<:Complex
+            if cmplx
+                rwork = Vector{$relty}(undef, job == 'N' ? 7*minmn : minmn*max(5*minmn+7, 2*max(m,n)+2*minmn+1))
+            end
+            iwork  = Vector{BlasInt}(undef, 8*minmn)
+            info   = Ref{BlasInt}()
+            for i = 1:2  # first call returns lwork as work[1]
+                if cmplx
+                    ccall((@blasfunc($gesdd), LinearAlgebra.LAPACK.liblapack), Cvoid,
+                          (Ref{UInt8}, Ref{BlasInt}, Ref{BlasInt}, Ptr{$elty},
+                           Ref{BlasInt}, Ptr{$relty}, Ptr{$elty}, Ref{BlasInt},
+                           Ptr{$elty}, Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt},
+                           Ptr{$relty}, Ptr{BlasInt}, Ptr{BlasInt}),
+                          job, m, n, A, max(1,stride(A,2)), S, U, max(1,stride(U,2)), VT, max(1,stride(VT,2)),
+                          work, lwork, rwork, iwork, info)
+                else
+                    ccall((@blasfunc($gesdd), LinearAlgebra.LAPACK.liblapack), Cvoid,
+                          (Ref{UInt8}, Ref{BlasInt}, Ref{BlasInt}, Ptr{$elty},
+                           Ref{BlasInt}, Ptr{$elty}, Ptr{$elty}, Ref{BlasInt},
+                           Ptr{$elty}, Ref{BlasInt}, Ptr{$elty}, Ref{BlasInt},
+                           Ptr{BlasInt}, Ptr{BlasInt}),
+                          job, m, n, A, max(1,stride(A,2)), S, U, max(1,stride(U,2)), VT, max(1,stride(VT,2)),
+                          work, lwork, iwork, info)
+                end
+                LinearAlgebra.LAPACK.chklapackerror(info[])
+                if i == 1
+                    # Work around issue with truncated Float32 representation of lwork in
+                    # sgesdd by using nextfloat. See
+                    # http://icl.cs.utk.edu/lapack-forum/viewtopic.php?f=13&t=4587&p=11036&hilit=sgesdd#p11036
+                    # and
+                    # https://github.com/scipy/scipy/issues/5401
+                    lwork = round(BlasInt, nextfloat(real(work[1])))
+                    resize!(work, lwork)
+                end
+            end
+            if job == 'O'
+                if m >= n
+                    return (A, S, VT)
+                else
+                    # ()__
+                    # ||::Z__
+                    # ||::|:::Z____
+                    # ||::|:::|====|
+                    # ||==|===|====|
+                    # ||""|===|====|
+                    # ||  `"""|====|
+                    # ||      `""""`
+                    return (U, S, A)
+                end
+            end
+            return (U, S, VT)
+        end
+    end
+end
+
+function LinearAlgebra.LinearAlgebra.svd!(A::StridedMatrix{T}, buffer::SVD;
+        full::Bool = false, alg::LinearAlgebra.Algorithm = LinearAlgebra.default_svd_alg(A)) where {T<:BlasFloat, Tr<:Real}
+    m,n = size(A)
+    if m == 0 || n == 0
+        u,s,vt = (Matrix{T}(I, m, full ? m : n), real(zeros(T,0)), Matrix{T}(I, n, n))
+    else
+        u,s,vt = _svd!(A,buffer,full,alg)
+    end
+    SVD(u,s,vt)
+end
+
+_svd!(A::StridedMatrix{T}, buffer::SVD, full::Bool, alg::LinearAlgebra.DivideAndConquer) where T<:BlasFloat = gesdd!(full ? 'A' : 'S', A, buffer.U, buffer.S, buffer.Vt)
